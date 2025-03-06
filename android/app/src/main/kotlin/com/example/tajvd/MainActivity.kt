cf9e4b25c29d6d9e412708acd2f39c8070b9f418
sanzhar <sanzzzsanzhar@gmail.com>
2025-03-06 23:27:34 +0500

    The first version of this project

---
+++ android/app/src/main/kotlin/com/example/tajvd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tajvd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/tajvd_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'r';
+
+class TajvdApp extends StatefulWidget {
+  const TajvdApp({super.key});
+
+  @override
+  State<TajvdApp> createState() => _TajvdAppState();
+}
+
+class _TajvdAppState extends State<TajvdApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(
+    ),theme: ,);
+  }
+}
