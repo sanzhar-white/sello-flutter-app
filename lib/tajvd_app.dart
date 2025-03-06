9ef7517a5a754151d25b3e43642d275a83de2327
sanzhar <sanzzzsanzhar@gmail.com>
2025-03-06 23:31:51 +0500

    first version of project

--- lib/tajvd_app.dart
+++ lib/tajvd_app.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'r';
 
-class TajvdApp extends StatefulWidget {
+class TajvdApp extends StatelessWidget {
   const TajvdApp({super.key});
 
   @override
-  State<TajvdApp> createState() => _TajvdAppState();
-}
-
-class _TajvdAppState extends State<TajvdApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-    ),theme: ,);
+    return MaterialApp(home: Scaffold());
   }
 }
